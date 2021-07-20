db0892f155af2270804db46f6dd9de24f773a6fa
Rushabhsoni1306 <64094208+Rushabh1306@users.noreply.github.com>
2021-07-20 14:31:55 +0530

    Updated District Search

--- cowin_vaccine/cowin/urls.py
+++ cowin_vaccine/cowin/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from django.urls.conf import include
-from .views import indexView
+from .views import indexView, notificationView
 urlpatterns = [
-    path('', indexView,name = ""),
+    path('', indexView,name = "index"),
+    path('notification', notificationView,name = "notification"),
 ]
--- cowin_vaccine/cowin/views.py
+++ cowin_vaccine/cowin/views.py
@@ -1,6 +1,6 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render
-from .utils import forPincode
+from .utils import forDistrict, forPincode
 from .models import StatesList, DistrictList
 
 # Create your views here.
@@ -12,11 +12,15 @@ def indexView(request):
     context['districts'] = districts
     data = None
     if request.method=='POST':
-        print("Post method")
+        print(request.POST)
         if request.POST['pin']:
             data = forPincode(int(request.POST['pin']))
         if request.POST['dist']:
-            print('District')
+            data = forDistrict(request.POST['dist'])
     context['data']=data
     print(data)
     return render(request,'index.html',context)
+
+def notificationView(request):
+    context = {}
+    return render(request,'notifications.html',context)
